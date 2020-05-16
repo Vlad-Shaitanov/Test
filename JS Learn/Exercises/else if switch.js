e18cb4d7177cf23c041411fb72be4e16a1486955
Vladislav <kelsenelenelvial@mail.ru>
2020-05-16 23:23:23 +0300

    Добавил папку для упражнений с JS. Добавил локальные шрифты в блог

---
+++ JS Learn/Exercises/else if switch.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// if (2 * 3 == 7) {
+// 	console.log("ВЕРНЫЙ ОТВЕТ")
+// } else {
+// 	console.log("НЕВЕРНО!")
+// }
+
+// for (let count = 1; count < 5; count++) {
+// 	console.log(`Сейчас переменная равна: ${count}`)
+// }
+
+// let num = 50;
+// if (num < 49) {
+// 	console.log("Неверно!");
+// } else if (num > 100) {
+// 	console.log("Много!");
+// } else {
+// 	console.log("Верно!");
+// }
+
+// (num == 50) ? console.log("Верно!") : console.log("Неверно!");
+
+// switch (num) {
+// 	case num < 49:
+// 		console.log("Неверно!");
+// 		break;
+// 	case num > 100:
+// 		console.log("Много!");
+// 		break;
+// 	case num > 80:
+// 		console.log("Много!");
+// 		break;
+// 	case 50:
+// 		console.log("Верно!");
+// 		break;
+// 	default:
+// 		console.log("Что-то пошло не так!");
+// 		break;
+// }
+
+// let name = prompt("What's the official name of JavaScript?", "");
+
+// if (name == "ECMAScript") {
+// 	alert("Well done!");
+// } else {
+// 	alert("You don't know? ECMAScript!");
+// }
+
+// let name = prompt("Введите число", "");
+
+// if (name > 0) {
+// 	alert("1");
+// } else if (name < 0) {
+// 	alert("-1");
+// } else {
+// 	alert("0");
+// }
+
+// result = (a + b < 4) ? 'Мало' : 'Много';
+
+// let login = prompt("Insert login");
+// let message = (login == 'Сотрудник') ? 'Привет' :
+// 	(login == 'Директор') ? 'Здравствуйте' :
+// 		(login == '') ? 'Нет логина' : '';
+// alert(message);
