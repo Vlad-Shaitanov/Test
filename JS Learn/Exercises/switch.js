503a754500c03ad1e9fca16f8645dc277b5831f3
Vladislav <kelsenelenelvial@mail.ru>
2020-05-21 23:25:00 +0300

    Начал редактировать блок Main

---
+++ JS Learn/Exercises/switch.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// let age = prompt("ghffhf", "");
+
+// switch (+age) {
+// 	case 15: alert("Мало!");
+// 		break;
+// 	case 20: alert("Угадал!");
+// 		break;
+// 	case 30: alert("Перебор!");
+// 		break;
+// 	default: alert("Нет таких значений!");
+// 		break;
+// }
+
+// Задание  на switch
+
+// let browser = prompt("What is your favorite browser?", "");
+
+// if (browser == "Edge") {
+// 	alert("You've got the Edge!");
+// } else if (browser == "Chrome" ||
+// 	browser == "Firefox" ||
+// 	browser == "Safari" ||
+// 	browser == "Opera") {
+// 	alert('Okay we support these browsers too');
+// } else {
+// 	alert('We hope that this page looks ok!');
+// }
+
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch (number) {
+// 	case 0: alert('Вы ввели число 0');
+// 		break;
+// 	case 1: alert('Вы ввели число 1');
+// 		break;
+// 	case 2:
+// 	case 3: alert('Вы ввели число 2, а может и 3');
+// 		break;
+
+// }
---
+++ JS Learn/TASKS/(Task_1)MyBudget/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false
+};
+
+alert(appData.budget);
