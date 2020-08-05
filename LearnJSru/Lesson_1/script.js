0abef4ef416c5f952269c517dc48fb196a67c824
Vladislav <kelsenelenelvial@mail.ru>
2020-08-05 21:33:49 +0300

    new exercises

---
+++ LearnJSru/Lesson_1/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let age = +prompt("How old are you?");
+if (age < 18) {
+	alert("Hello, bro");
+} else if (age > 70 && age < 100) {
+	alert("Great!!!");
+} else {
+	alert("Good morning)");
+}
