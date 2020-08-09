f63492b64504958f64c9fdfcb781def1b227c371
Vladislav <kelsenelenelvial@mail.ru>
2020-08-09 21:11:25 +0300

    new exercise

---
+++ LearnJSru/Lesson_6/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Унарный минус (меняет знак числа на противоположный)
+let x = 1;
+x = - x;
+console.log(x);
+
+// Унарный плюс
+let apples = "2";
+let oranges = "3";
+console.log(+apples + +oranges);
+
+//----------TASKS
+
+let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+// a = 2;
+// b = 2;
+// c = 2;
+// d = 1;
+
+let e = 2;
+let r = 1 + (e *= 2);
+// e = 4;
+// r = 5;
