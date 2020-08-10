5bf1739988ee08cbf6efed9956ae52c6e9ee662c
Vladislav <kelsenelenelvial@mail.ru>
2020-08-10 22:12:21 +0300

    new exercise

---
+++ LearnJSru/Lesson_8/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// let age = prompt("сколько тебе лет?", "");
+// alert(`Тебе ${age} лет!`);
+
+// let question = confirm("Вам уже есть 18 лет?");
+// console.log(question);
+
+
+//----------TASKS
+
+let userName = prompt("Как Вас зовут?", "Имя");
+alert(`Приятно познакомиться, ${userName}=)`);
---
+++ LearnJSru/Lesson_9/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//----------TASKS
+
+//1)
+// if ("0") {
+// 	alert('Привет');
+// }
+
+//2)
+// let jsName = prompt("What's the 'официальное название' JavaScript?", "");
+// if (jsName == "ECMAScript") {
+// 	alert("Правильно!");
+// } else {
+// 	alert("You don't know? “ECMAScript”!");
+// }
+
+//3)
+// let number = prompt("Введите число", "");
+// if (number > 0) {
+// 	alert(1);
+// } else if (number < 0) {
+// 	alert(-1);
+// } else {
+// 	alert(0);
+// }
+
+//4)
+// let result;
+// result == (a + b > 4) ? "Мало" : "Много";
+
+//5)
+// let message;
+// let login = prompt("Представьтесь, пожалуйста", "");
+// message = (login == 'Сотрудник') ? alert('Привет') :
+// 	(login == 'Директор') ? alert('Здравствуйте') :
+// 		(login == '') ? alert('Нет логина') : alert('');
