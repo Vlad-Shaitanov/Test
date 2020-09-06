34907386bba3cfb387777b9c92a376a797b1fd9e
Vladislav <kelsenelenelvial@mail.ru>
2020-09-06 22:38:28 +0300

    Codewars upd

--- Codewars/8_Kyu/script.js
+++ Codewars/8_Kyu/script.js
@@ -130,3 +130,36 @@ function litres(time) {
 	}
 }
 console.log(litres(3));
+
+
+/*Description:
+Make a simple function called greet that returns the most-famous "hello world!"
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create
+the most creative hello world you can think of? What is a "hello world"
+solution you would want to show your friends?*/
+function greet() {
+	let hi = "hello world!";
+	return hi;
+}
+console.log(greet());
+
+
+
+/*In this simple exercise, you will build a program that takes a value,
+integer, and returns a list of its multiples up to another value, limit. If
+limit is a multiple of integer, it should be included as well. There will
+only ever be positive integers passed into the function, not consisting of 0.
+The limit will always be higher than the base.
+For example, if the parameters passed are (2, 6), the function should return
+[2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.*/
+function findMultiples(integer, limit) {
+	let arr = [];
+	for (let i = integer; i <= limit; i++) {
+		if (i % integer == 0) {
+			arr.push(i);
+		}
+	}
+	return arr;
+}
+console.log(findMultiples(4, 27));
