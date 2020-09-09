5935322c91f5ba17b4d4eb9445ef94351c118b17
Vladislav <kelsenelenelvial@mail.ru>
2020-09-09 22:46:16 +0300

    New lesson

---
+++ JS+React(Petrich)/Lessons/Getting_elements_from_the_page/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let box = document.getElementById("box"),
+	btn = document.getElementsByTagName("button"),
+	cirlce = document.getElementsByClassName("circle"),
+	heart = document.querySelectorAll(".heart"),
+	oneHeart = document.querySelector(".heart");
+
+console.log(box);
+console.log(btn);
+console.log(btn[0]);
+console.log(cirlce[2]);
+console.log(heart);
+console.log(heart[1]);
+console.log(oneHeart);
