88378ef8bc21a456ca45a85f7e91faefae384579
Vladislav <kelsenelenelvial@mail.ru>
2020-09-11 15:55:05 +0300

    Lesson upd

--- JS+React(Petrich)/Lessons/Getting_elements_from_the_page/script.js
+++ JS+React(Petrich)/Lessons/Getting_elements_from_the_page/script.js
@@ -6,10 +6,29 @@ let box = document.getElementById("box"),
 	heart = document.querySelectorAll(".heart"),
 	oneHeart = document.querySelector(".heart");
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(cirlce[2]);
-console.log(heart);
-console.log(heart[1]);
-console.log(oneHeart);
+// console.log(box);
+// console.log(btn);
+// console.log(btn[0]);
+// console.log(cirlce[2]);
+// console.log(heart);
+// console.log(heart[1]);
+// console.log(oneHeart);
+
+//Теперь применим новые стили к эелементам, полученным со страницы
+box.onclick = function boxClick() {
+	box.style.backgroundColor = "blue";
+	box.style.transition = "0.5";
+};
+btn[1].style.borderRadius = "100%";
+
+cirlce[0].style.backgroundColor = "green";
+cirlce[1].style.backgroundColor = "yellow";
+cirlce[2].style.backgroundColor = "red";
+
+// for (let i = 0; i < heart.length; i++) {
+// 	heart[i].style.backgroundColor = ("orange");
+// }
+
+heart.forEach(function (item, i, hearts) {
+	item.style.backgroundColor = ("orange");
+});
