f6e6c4447161b36d0e098bdd5cc1e7d82717da7f
Vladislav <kelsenelenelvial@mail.ru>
2020-09-15 22:38:17 +0300

    new Task

---
+++ JS+React(Petrich)/Project/Task_7/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let start = document.querySelector("#start"),
+	budgetVal = document.querySelector(".budget-value"),
+	daybudgetVal = document.querySelector(".daybudget-value"),
+	levelVal = document.querySelector(".level-value"),
+	expensesVal = document.querySelector(".expenses-value"),
+	optionalExpensesVal = document.querySelector(".optionalexpenses-value"),
+	incomeVal = document.querySelector(".income-value"),
+	monthsavingsVal = document.querySelector(".monthsavings-value"),
+	yearsavingsVal = document.querySelector(".yearsavings-value"),
+	expensesItem = document.querySelectorAll(".expenses-item");
