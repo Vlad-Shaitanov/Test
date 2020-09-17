83824025e7ab0c3838d6632d8b16d798beef0151
Vladislav <kelsenelenelvial@mail.ru>
2020-09-17 20:38:10 +0300

    lesson upd

--- JS+React(Petrich)/Lessons/Events_EventListeners/script.js
+++ JS+React(Petrich)/Lessons/Events_EventListeners/script.js
@@ -1,17 +1,41 @@
 "use strict";
 
-let btn = document.getElementsByTagName("button");
+let btn = document.querySelectorAll("button"),
+	wrap = document.querySelector(".wrapper"),
+	link = document.querySelector("a");
 
 // btn[0].onclick = function () {
 // 	alert("Вы нажали первую кнопку!");
 // };
 
-btn[0].addEventListener("click", function () {
-	alert("Вы нажали первую кнопку!");
+// btn[0].addEventListener("click", function (event) {
+// 	// let target = event.target;
+// 	// target.style.display = "none";
+// 	// console.log(event);
+// 	console.log("Произошло событие: " + event.type + " на элементе - " +
+// 		event.target);
+// });
+
+// wrap.addEventListener("click", function (event) {
+// 	console.log("Произошло событие: " + event.type + " на элементе - " +
+// 		event.target);
+// });
+
+link.addEventListener("click", function (event) {
+	event.preventDefault();
+	console.log("Произошло событие: " + event.type + " на элементе - " +
+		event.target);
 });
-btn[0].addEventListener("click", function () {
-	alert("Вы снова нажали первую кнопку!");
+
+btn.forEach(function (item) {
+	item.addEventListener("mouseleave", function () {
+		console.log("Вышли за пределы элемента!");
+	});
 });
-btn[0].addEventListener("mouseenter", function () {
-	alert("Курсок щекочет кнопку=)");
-});
+
+// btn[0].addEventListener("click", function () {
+// 	alert("Вы снова нажали первую кнопку!");
+// });
+// btn[0].addEventListener("mouseenter", function () {
+// 	alert("Курсор щекочет кнопку=)");
+// });
