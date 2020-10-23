5bdb099c7c9ee71ea7e8c89e165cf4254420f442
Vladislav <kelsenelenelvial@mail.ru>
2020-10-23 21:50:58 +0300

    timer on page

--- JS+React(Petrich)/project_YOGA/js/main.js
+++ JS+React(Petrich)/project_YOGA/js/main.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
+	//===TABs===
 	let tab = document.querySelectorAll(".info-header-tab"),
 		info = document.querySelector(".info-header"),
 		tabContent = document.querySelectorAll(".info-tabcontent");
@@ -38,4 +39,52 @@ window.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	});
+
+	//===Timer===
+	let deadline = "2020-10-24";//Когда истекает таймер
+
+	function getTimeRemaining(endtime) {
+		//Разница между дедлайном и текущим временем
+		let time = Date.parse(deadline) - Date.parse(new Date());
+		let seconds = Math.floor((time / 1000) % 60),
+			minutes = Math.floor((time / 1000 / 60) % 60),
+			hours = Math.floor((time / (1000 * 60 * 60)));
+		return {
+			"total": time,
+			"hours": hours,
+			"minutes": minutes,
+			"seconds": seconds
+		};
+	}
+
+	function setClock(id, endtime) {//Передали id элемента и дедлайн
+		let timer = document.getElementById(id),
+			hours = timer.querySelector(".hours"),
+			minutes = timer.querySelector(".minutes"),
+			seconds = timer.querySelector(".seconds");
+		let timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+
+			function addZero(num) {
+				if (num <= 9) {
+					return "0" + num;
+				} else {
+					return num;
+				}
+			}
+			hours.textContent = addZero(t.hours);
+			minutes.textContent = addZero(t.minutes);
+			seconds.textContent = addZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
+			}
+		}
+	}
+	setClock("timer", deadline);
 });
